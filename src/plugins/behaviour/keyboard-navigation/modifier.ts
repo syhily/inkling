aa102b5f0975948b9a36e8b43491b9b49f5276bf
Yufan Sheng <syhily@gmail.com>
2026-07-14 01:54:36 +0800

    refactor(plugins): extract modifier-key handler from registerKeyboardNavigation

---
+++ src/plugins/behaviour/keyboard-navigation/modifier.ts
@@ -0,0 +1,151 @@
+import type { LexicalEditor } from 'lexical'
+
+import { $isListNode, INSERT_ORDERED_LIST_COMMAND, INSERT_UNORDERED_LIST_COMMAND } from '@lexical/list'
+import { $createHeadingNode, $createQuoteNode, $isQuoteNode } from '@lexical/rich-text'
+import { $setBlocksType } from '@lexical/selection'
+import {
+  $createParagraphNode,
+  $getRoot,
+  $getSelection,
+  $isDecoratorNode,
+  $isNodeSelection,
+  $isParagraphNode,
+  $isRangeSelection,
+  COMMAND_PRIORITY_LOW,
+  FORMAT_TEXT_COMMAND,
+  KEY_MODIFIER_COMMAND,
+} from 'lexical'
+
+import { $createAsideNode, $isAsideNode } from '@/nodes/AsideNode'
+import { $selectDecoratorNode } from '@/utils'
+
+import type { KeyboardNavigationDeps } from './types'
+
+export function registerModifierCommand(editor: LexicalEditor, _deps: KeyboardNavigationDeps): () => void {
+  return editor.registerCommand(
+    KEY_MODIFIER_COMMAND,
+    (event) => {
+      const { altKey, ctrlKey, metaKey, shiftKey, code, key } = event
+      const isArrowUp = key === 'ArrowUp' || event.keyCode === 38
+      const isArrowDown = key === 'ArrowDown' || event.keyCode === 40
+
+      if (metaKey && (isArrowUp || isArrowDown)) {
+        const selection = $getSelection()
+        const isNodeSelected = $isNodeSelection(selection)
+        const firstChild = $getRoot().getFirstChild()
+        const lastChild = $getRoot().getLastChild()
+        const hasCardAtStart = firstChild !== null && $isDecoratorNode(firstChild)
+        const hasCardAtEnd = lastChild !== null && $isDecoratorNode(lastChild)
+
+        if (isNodeSelected || hasCardAtStart || hasCardAtEnd) {
+          // meta+down on macos moves cursor to end of document
+          if (isArrowDown) {
+            event.preventDefault()
+
+            if (lastChild !== null) {
+              if ($isDecoratorNode(lastChild)) {
+                $selectDecoratorNode(lastChild)
+                return true
+              } else {
+                lastChild.selectEnd()
+                return true
+              }
+            }
+          }
+
+          // meta+up on macos moves cursor to start of document
+          if (isArrowUp) {
+            event.preventDefault()
+
+            if (firstChild !== null) {
+              if ($isDecoratorNode(firstChild)) {
+                $selectDecoratorNode(firstChild)
+                return true
+              } else {
+                firstChild.selectStart()
+                return true
+              }
+            }
+          }
+        }
+      }
+
+      if (ctrlKey && code === 'KeyQ') {
+        // avoid quit behaviour
+        event.preventDefault()
+
+        const selection = $getSelection()
+        if ($isRangeSelection(selection)) {
+          const firstNode = selection?.anchor.getNode().getTopLevelElement()
+
+          if ($isParagraphNode(firstNode)) {
+            $setBlocksType(selection, () => $createQuoteNode())
+          } else if ($isQuoteNode(firstNode)) {
+            $setBlocksType(selection, () => $createAsideNode())
+          } else if ($isAsideNode(firstNode)) {
+            $setBlocksType(selection, () => $createParagraphNode())
+          }
+        }
+      }
+
+      // Ctrl+Option+H to toggle highlight
+      if ((ctrlKey || metaKey) && altKey && code === 'KeyH') {
+        event.preventDefault()
+        editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'highlight')
+        return true
+      }
+
+      // ctrl shift K should format text as code
+      if (ctrlKey && shiftKey && code === 'KeyK') {
+        editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'code')
+        return true
+      }
+
+      // ctrl alt U should strikethrough (cmd alt U launches the browser source view)
+      if (ctrlKey && altKey && code === 'KeyU') {
+        editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'strikethrough')
+        return true
+      }
+
+      // ctrl alt 1-6 should create headings
+      const HEADING_TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as const
+      if (ctrlKey && altKey && key && /^[1-6]$/.test(key)) {
+        event.preventDefault()
+
+        const selection = $getSelection()
+        if ($isRangeSelection(selection)) {
+          const headingIndex = Number(key) - 1
+          $setBlocksType(selection, () => $createHeadingNode(HEADING_TAGS[headingIndex]))
+        }
+      }
+
+      if (ctrlKey && code === 'KeyL') {
+        event.preventDefault()
+
+        const selection = $getSelection()
+        if ($isRangeSelection(selection)) {
+          const firstNode = selection?.anchor.getNode().getTopLevelElement()
+
+          if ($isListNode(firstNode)) {
+            editor.update(() => {
+              const pNode = $createParagraphNode()
+              $setBlocksType(selection, () => pNode)
+
+              // Lexical will automatically indent the paragraph node to the
+              // list item level but we don't allow indented paragraphs
+              pNode.setIndent(0)
+            })
+          } else {
+            if (altKey) {
+              editor.dispatchCommand(INSERT_ORDERED_LIST_COMMAND, undefined)
+            } else {
+              editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND, undefined)
+            }
+          }
+        }
+      }
+      return false
+    },
+    COMMAND_PRIORITY_LOW,
+  )
+}
--- src/plugins/behaviour/registerKeyboardNavigation.ts
+++ src/plugins/behaviour/registerKeyboardNavigation.ts
@@ -1,14 +1,12 @@
 import type { LexicalEditor } from 'lexical'
 
 import { $isLinkNode } from '@lexical/link'
-import { $isListItemNode, $isListNode, INSERT_ORDERED_LIST_COMMAND, INSERT_UNORDERED_LIST_COMMAND } from '@lexical/list'
-import { $createHeadingNode, $createQuoteNode, $isQuoteNode } from '@lexical/rich-text'
-import { $setBlocksType } from '@lexical/selection'
+import { $isListItemNode } from '@lexical/list'
+import { $isQuoteNode } from '@lexical/rich-text'
 import { mergeRegister } from '@lexical/utils'
 import {
   $createNodeSelection,
   $createParagraphNode,
-  $getRoot,
   $getSelection,
   $isDecoratorNode,
   $isElementNode,
@@ -20,16 +18,14 @@ import {
   $setSelection,
   COMMAND_PRIORITY_LOW,
   DELETE_LINE_COMMAND,
-  FORMAT_TEXT_COMMAND,
   INSERT_PARAGRAPH_COMMAND,
   KEY_BACKSPACE_COMMAND,
   KEY_DELETE_COMMAND,
   KEY_ESCAPE_COMMAND,
-  KEY_MODIFIER_COMMAND,
   KEY_TAB_COMMAND,
 } from 'lexical'
 
-import { $createAsideNode, $isAsideNode } from '@/nodes/AsideNode'
+import { $isAsideNode } from '@/nodes/AsideNode'
 import { $createCodeBlockNode } from '@/nodes/CodeBlockNode'
 import { $isAtTopOfNode, $selectDecoratorNode } from '@/utils'
 
@@ -44,6 +40,7 @@ import {
 } from './keyboard-navigation/arrows'
 import { registerEnterCommand } from './keyboard-navigation/enter'
 import { registerKeyDownPassthrough } from './keyboard-navigation/key-down'
+import { registerModifierCommand } from './keyboard-navigation/modifier'
 import { RANGE_TO_ELEMENT_BOUNDARY_THRESHOLD_PX, SPECIAL_MARKUPS } from './utils'
 
 export function registerKeyboardNavigation(editor: LexicalEditor, deps: KeyboardNavigationDeps) {
@@ -56,132 +53,7 @@ export function registerKeyboardNavigation(editor: LexicalEditor, deps: Keyboard
     registerArrowDownCommand(editor, deps),
     registerArrowLeftCommand(editor, deps),
     registerArrowRightCommand(editor, deps),
-    editor.registerCommand(
-      KEY_MODIFIER_COMMAND,
-      (event) => {
-        const { altKey, ctrlKey, metaKey, shiftKey, code, key } = event
-        const isArrowUp = key === 'ArrowUp' || event.keyCode === 38
-        const isArrowDown = key === 'ArrowDown' || event.keyCode === 40
-
-        if (metaKey && (isArrowUp || isArrowDown)) {
-          const selection = $getSelection()
-          const isNodeSelected = $isNodeSelection(selection)
-          const firstChild = $getRoot().getFirstChild()
-          const lastChild = $getRoot().getLastChild()
-          const hasCardAtStart = firstChild !== null && $isDecoratorNode(firstChild)
-          const hasCardAtEnd = lastChild !== null && $isDecoratorNode(lastChild)
-
-          if (isNodeSelected || hasCardAtStart || hasCardAtEnd) {
-            // meta+down on macos moves cursor to end of document
-            if (isArrowDown) {
-              event.preventDefault()
-
-              if (lastChild !== null) {
-                if ($isDecoratorNode(lastChild)) {
-                  $selectDecoratorNode(lastChild)
-                  return true
-                } else {
-                  lastChild.selectEnd()
-                  return true
-                }
-              }
-            }
-
-            // meta+up on macos moves cursor to start of document
-            if (isArrowUp) {
-              event.preventDefault()
-
-              if (firstChild !== null) {
-                if ($isDecoratorNode(firstChild)) {
-                  $selectDecoratorNode(firstChild)
-                  return true
-                } else {
-                  firstChild.selectStart()
-                  return true
-                }
-              }
-            }
-          }
-        }
-
-        if (ctrlKey && code === 'KeyQ') {
-          // avoid quit behaviour
-          event.preventDefault()
-
-          const selection = $getSelection()
-          if ($isRangeSelection(selection)) {
-            const firstNode = selection?.anchor.getNode().getTopLevelElement()
-
-            if ($isParagraphNode(firstNode)) {
-              $setBlocksType(selection, () => $createQuoteNode())
-            } else if ($isQuoteNode(firstNode)) {
-              $setBlocksType(selection, () => $createAsideNode())
-            } else if ($isAsideNode(firstNode)) {
-              $setBlocksType(selection, () => $createParagraphNode())
-            }
-          }
-        }
-
-        // Ctrl+Option+H to toggle highlight
-        if ((ctrlKey || metaKey) && altKey && code === 'KeyH') {
-          event.preventDefault()
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'highlight')
-          return true
-        }
-
-        // ctrl shift K should format text as code
-        if (ctrlKey && shiftKey && code === 'KeyK') {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'code')
-          return true
-        }
-
-        // ctrl alt U should strikethrough (cmd alt U launches the browser source view)
-        if (ctrlKey && altKey && code === 'KeyU') {
-          editor.dispatchCommand(FORMAT_TEXT_COMMAND, 'strikethrough')
-          return true
-        }
-
-        // ctrl alt 1-6 should create headings
-        const HEADING_TAGS = ['h1', 'h2', 'h3', 'h4', 'h5', 'h6'] as const
-        if (ctrlKey && altKey && key && /^[1-6]$/.test(key)) {
-          event.preventDefault()
-
-          const selection = $getSelection()
-          if ($isRangeSelection(selection)) {
-            const headingIndex = Number(key) - 1
-            $setBlocksType(selection, () => $createHeadingNode(HEADING_TAGS[headingIndex]))
-          }
-        }
-
-        if (ctrlKey && code === 'KeyL') {
-          event.preventDefault()
-
-          const selection = $getSelection()
-          if ($isRangeSelection(selection)) {
-            const firstNode = selection?.anchor.getNode().getTopLevelElement()
-
-            if ($isListNode(firstNode)) {
-              editor.update(() => {
-                const pNode = $createParagraphNode()
-                $setBlocksType(selection, () => pNode)
-
-                // Lexical will automatically indent the paragraph node to the
-                // list item level but we don't allow indented paragraphs
-                pNode.setIndent(0)
-              })
-            } else {
-              if (altKey) {
-                editor.dispatchCommand(INSERT_ORDERED_LIST_COMMAND, undefined)
-              } else {
-                editor.dispatchCommand(INSERT_UNORDERED_LIST_COMMAND, undefined)
-              }
-            }
-          }
-        }
-        return false
-      },
-      COMMAND_PRIORITY_LOW,
-    ),
+    registerModifierCommand(editor, deps),
     // backspace when card isn't selected
     editor.registerCommand(
       KEY_BACKSPACE_COMMAND,
